f1990674575bad010e0a093275fc4a43841ad1f4
madeline-monaco <madeline.d.monaco@gmail.com>
2017-12-10 10:35:43 -0500

    week-4 homework
    Startup website and project proposal

---
+++ class-work/class07/first-javascript/script.js
@@ -0,0 +1,20 @@
+var product = 10*10;
+var quotient = 100/10;
+var difference = 50-17;
+var sum = 50+17;
+var name = "Madeline" + " Monaco";
+
+console.log(product + quotient + difference + sum)
+
+function onra () {
+    var city = "Saigon";
+    console.log(`Last Tango In ${city}`)
+}
+
+onra();
+
+console.log(document.getElementById("Hello"));
+
+var hello = document.getElementById ("hello");
+
+hello.style.color = "tomato";
