dc6ee05387b6c42357a5d605d3d24806ed3622e1
madeline-monaco <madeline.d.monaco@gmail.com>
2018-01-21 16:27:24 -0500

    final project
    java pseudo code

---
+++ final-project/app.js
@@ -0,0 +1,11 @@
+// Decide between zip code and borough
+
+// Select a date
+
+// Click submit
+
+// If zip code, show map of 10 open markets near there.
+
+// If borough, show map of all open markets there.
+
+// Once market is selected, show hours, location, and info. 
